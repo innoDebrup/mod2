e261aeb73f1132129b4e411a9898da262edf1ed8
innoDebrup <debrup.pramanik@innoraft.com>
2024-02-22 21:39:44 +0530

    FT2-40: Changed CSS as requested. Added validation checks in inputs.

--- basic1/index.php
+++ basic1/index.php
@@ -1,3 +1,10 @@
+<?php
+$fullName = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fullName = $_POST["firstName"] . " " . $_POST["lastName"];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,51 +13,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basic Assignment 1</title>
 
-    <!--  Linked external CSS file. -->
+    <!-- Linked external CSS file. -->
     <link rel="stylesheet" href="style.css" type="text/css">
 
 </head>
 
 <body>
-
-    <!-- PHP to handle Post. -->
-    <?php
-    $fullName = " ";
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $fullName = $_POST["firstName"] . " " . $_POST["lastName"];
-    }
-    ?>
-    <!-- Main Form -->
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <fieldset>
-
-            <legend>Personal Details</legend>
-
-            <!-- First-name input field. -->
-            <label for="firstName">First Name: </label>
-            <input type="text" name="firstName" placeholder="Enter your first name" oninput=update() id="fname">
-            <!-- Last-name input field. -->
-            <label for="lastName">Last Name: </label>
-            <input type="text" name="lastName" placeholder="Enter your last name" oninput=update() id="lname">
-            <!-- Full-name input field (Disabled/Non-editable). -->
-            <label for="fullName">Full Name: </label>
-            <input type="text" name="fullName" id="fullName" disabled></p>
-            <!-- Submit button. -->
-            <input type="submit" value="submit" name="submit">
-
-        </fieldset>
-    </form>
+    <!-- Main Form. -->
+    <div class="form">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+            <fieldset>
+                <legend>Personal Details</legend>
+                <!-- First-name input field. -->
+                <label for="firstName">First Name: </label>
+                <input type="text" name="firstName" pattern="[A-Za-z]+" placeholder="Enter your first name" oninput=update() id="fname">
+                <!-- Last-name input field. -->
+                <label for="lastName">Last Name: </label>
+                <input type="text" name="lastName" pattern="[A-Za-z]+" placeholder="Enter your last name" oninput=update() id="lname">
+                <!-- Full-name input field (Disabled/Non-editable). -->
+                <label for="fullName">Full Name: </label>
+                <input type="text" name="fullName" id="fullName" disabled>
+                <!-- Submit button. -->
+                <input type="submit" value="submit" name="submit">
+            </fieldset>
+        </form>
+    </div>
     <!-- Greet the user once the form is submitted. -->
-    <h1>
-        <?php
-        if ($fullName !== " ") {
-            echo "Hello $fullName!";
-        }
-        ?>
-    </h1>
-    <!-- Include the greet  -->
+    <div class="greet">
+        <p class="message">
+            <?php
+            if ($fullName !== " " && $fullName !== "") {
+                echo "Hello $fullName!";
+            }
+            ?>
+        </p>
+    </div>
+    <!-- Include the script.  -->
     <script src="script.js"></script>
-
 </body>
 
 </html>
+
--- basic1/script.js
+++ basic1/script.js
@@ -1,7 +1,25 @@
 const fname = document.querySelector("#fname");
 const lname = document.querySelector("#lname");
 const fullName = document.querySelector("#fullName");
+const re = new RegExp("^[A-Za-z]+$");
 
 const update = function () {
-  fullName.value = `${fname.value} ${lname.value}`;
+  if(!re.test(fname.value)){
+    if(re.test(lname.value)){
+      fullName.value = `${lname.value}`;
+    }
+    else{
+      fullName.value = ``;
+    }
+  }
+  else {
+    if(re.test(lname.value)){
+      fullName.value = `${fname.value} ${lname.value}`;
+    }
+    else{
+      fullName.value = `${fname.value}`;
+    }
+  }
+
 };
+
