daa193f086cf102a7da1b355365664f040eccb3c
innoDebrup <debrup.pramanik@innoraft.com>
2024-02-28 10:39:19 +0530

    FT2-40: Reactivated email.php.

--- basic6/email.php
+++ basic6/email.php
@@ -1,5 +1,5 @@
 <?php
-//require "access.php";
+require "access.php";
 $email = $emailErrorMessage = $validError = "";
 $emailError = 0;
 $re = '/^[\w._]+@[\w]+(\.[a-z]{2,}){0,2}$/i';
@@ -13,26 +13,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $emailErrorMessage = "Invalid email address format!";
     }
 
-    // if ($emailError != 1) {
-    //     $ch = curl_init();
-    //     curl_setopt_array($ch, [
-    //         CURLOPT_URL => 'https://emailvalidation.abstractapi.com/v1/?api_key=' . $access_key . '&email=' . $email,
-    //         CURLOPT_RETURNTRANSFER => true,
-    //         CURLOPT_FOLLOWLOCATION => false
-    //     ]);
-    //     $response = curl_exec($ch);
-    //     $data = json_decode($response, true);
+    if ($emailError != 1) {
+        $ch = curl_init();
+        curl_setopt_array($ch, [
+            CURLOPT_URL => 'https://emailvalidation.abstractapi.com/v1/?api_key=' . $access_key . '&email=' . $email,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => false
+        ]);
+        $response = curl_exec($ch);
+        $data = json_decode($response, true);
 
-    //     if ($data["is_disposable_email"]["value"] === true) {
-    //         $emailError = 1;
-    //         $validError = "Disposable Emails are not allowed!";
-    //     }
-    //     elseif ($data['deliverability'] === 'UNDELIVERABLE') {
-    //         $emailError = 1;
-    //         $validError = "Invalid email as it is Undeliverable!";
-    //     }
-    //     else{
-    //         $emailError = 0;
-    //     }
-    // }
+        if ($data["is_disposable_email"]["value"] === true) {
+            $emailError = 1;
+            $validError = "Disposable Emails are not allowed!";
+        }
+        elseif ($data['deliverability'] === 'UNDELIVERABLE') {
+            $emailError = 1;
+            $validError = "Invalid email as it is Undeliverable!";
+        }
+        else{
+            $emailError = 0;
+        }
+    }
 }
