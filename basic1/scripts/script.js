c027fbe9f86b91aa54d3eef18a6c4d12eb1d3d1d
innoDebrup <debrup.pramanik@innoraft.com>
2024-03-01 21:25:04 +0530

    FT2-40: Added comments in the script file.

--- basic1/scripts/script.js
+++ basic1/scripts/script.js
@@ -3,16 +3,19 @@ const lastName = document.querySelector("#lastName");
 const fullName = document.querySelector("#fullName");
 const re = new RegExp("^[A-Za-z]+$");
 /**
- * This Function Updates the fullname
+ * This Function Updates the value of the disabled FullName input field.
  */
 const update = function () {
+  //Checks the firstName format according to the regex. If it fails then it enters the IF block.
   if (!re.test(firstName.value)) {
+    //If-else structure for lastName. The checks are performed on lastName.
     if (re.test(lastName.value)) {
       fullName.value = `${lastName.value}`;
     } else {
       fullName.value = ``;
     }
   } else {
+    //If-else structure for lastName. The checks are performed on lastName.
     if (re.test(lastName.value)) {
       fullName.value = `${firstName.value} ${lastName.value}`;
     } else {
