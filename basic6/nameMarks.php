2fb65e48d7edcf7b3f751fef0ffac012c0325522
innoDebrup <debrup.pramanik@innoraft.com>
2024-03-04 15:15:34 +0530

    FT2-40: Reworked file structure and added improvements.

--- basic6/nameMarks.php
+++ basic6/nameMarks.php
@@ -2,18 +2,23 @@
 $fullName = $marksInput = "";
 $marksArray = $marksFinal = $marksErrorMessage = [];
 $marksError = 0;
-$re = '/^[a-z]+[ ]*\|[ ]*[0-9]{1,3}$/i';    //Regex for validating Input Format of Marks.
+$re1 = '/^[a-z]+$/i'; // Regex to check all characters of the string are only alphabets.
+$re2 = '/^[a-z]+[ ]*\|[ ]*[0-9]{1,3}$/i';    // Regex for validating Input Format of Marks.
+$firstName = htmlspecialchars($_POST['firstName']);
+$lastName = htmlspecialchars($_POST['lastName']);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $fullName = $_POST["firstName"] . " " . $_POST["lastName"];
+    if (preg_match($re1, $firstName) && preg_match($re1, $lastName)) {
+        $fullName = $firstName . " " . $lastName;
+    }
     $marksInput = $_POST["marks"];
-    $marksArray = explode("\n", $marksInput);   //Exploding the string to form a string array. Point of exploding is a newline character.
+    $marksArray = explode("\n", $marksInput);   // Exploding the string to form a string array. Point of exploding is a newline character.
     $n = count($marksArray);
     for ($i = 0; $i < $n; $i++) {
-        $marksArray[$i] = trim($marksArray[$i]);    //Trimming any whitespaces at the beginning and end of the string.
-        $marksArray[$i] = preg_replace('/[ ]/', '', $marksArray[$i]);   //Cleaning up spaces in the string.
-        //----------Delete any string that does not match the given format.---------
-        if (!preg_match($re, $marksArray[$i])) {
+        $marksArray[$i] = trim($marksArray[$i]);    // Trimming any whitespaces at the beginning and end of the string.
+        $marksArray[$i] = preg_replace('/[ ]/', '', $marksArray[$i]);   // Cleaning up spaces in the string.
+        //---------- Set error if a string does not match the given format. ---------
+        if (!preg_match($re2, $marksArray[$i])) {
             $marksError = 1;
             array_push($marksErrorMessage, "Incorrect Marks Format at line " . $i + 1);
             unset($marksArray[$i]);
@@ -22,7 +27,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     $marksArray = array_values($marksArray);
     foreach ($marksArray as $x) {
-        array_push($marksFinal, explode("|", $x));  //The marksFinal is an array of string arrays, where each string array consists a subject name at 0th index and the marks at the 1st index.
+        array_push($marksFinal, explode("|", $x));
+        // The marksFinal is an array of string arrays, 
+        // where each string array consists a subject name at 0th index and the marks at the 1st index.
     }
 }
-?>
--- basic6/result.php
+++ basic6/result.php
@@ -3,7 +3,7 @@ require "upload.php";
 require "phoneNumber.php";
 require "email.php";
 require "nameMarks.php";
-if(!($uploadOk === 0 || $marksError === 1 || $emailError === 1)) {
+if (!($uploadOk === 0 || $marksError === 1 || $phoneError === 1 || $emailError === 1)) {
     require "pdf.php";
 }
 ?>
@@ -16,22 +16,27 @@ if(!($uploadOk === 0 || $marksError === 1 || $emailError === 1)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basic Assignment Output</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="./styles/style.css" type="text/css">
 </head>
 
 <body>
-
-    <div class="<?php echo ($uploadOk === 0 || $marksError === 1 || $emailError === 1) ? "error" : "hide"; ?>">
+    <!-- If any error occurs then show this div. -->
+    <div class="<?php echo ($uploadOk === 0 || $marksError === 1 || $phoneError === 1 || $emailError === 1) ? "error" : "hide"; ?>">
         <span>ERROR!</span>
         <p>
             <?php
             if ($uploadOk === 0) {
                 echo $errorMessage;
-            } elseif ($marksError === 1) {
+            }
+            elseif ($marksError === 1) {
                 foreach ($marksErrorMessage as $x) {
                     echo $x . nl2br("\n");
                 }
-            } else {
+            }
+            else if ($phoneError) {
+                echo $phoneErrorMessage;
+            }
+            else {
                 echo $emailErrorMessage;
                 echo $validError;
             }
@@ -39,6 +44,5 @@ if(!($uploadOk === 0 || $marksError === 1 || $emailError === 1)) {
         </p>
     </div>
 </body>
+
 </html>
-
-
